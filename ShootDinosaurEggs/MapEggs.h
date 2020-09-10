c88231e9f2d90cc1abd8f89c28d89d7dad776c28
thanhduc43 <thanhduc4320@gmail.com>
2020-09-10 21:59:18 +0700

    Add class MapEggs

---
+++ ShootDinosaurEggs/MapEggs.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef MAP_EGGS_H_
+#define MAP_EGGS_H_
+#include "SupportFunctions.h"
+#include "Egg.h"
+#include <vector>
+using namespace SDL_SupportFunc;
+
+class MapEggs
+{
+private:
+	vector<Egg>	_mapEggs;
+
+};
+
+
+#endif // !MAP_EGGS_H_
+
+class MapEggs
+{
+};
+
+
